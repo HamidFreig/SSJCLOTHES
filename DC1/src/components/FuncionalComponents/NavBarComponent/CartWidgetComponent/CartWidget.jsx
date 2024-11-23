73be3c607ef9a05a73d4114c8440a2420d88e046
Hamid Freig <110936554+HamidFreig@users.noreply.github.com>
2024-11-23 01:53:26 -0300

    se mejoro desing ademas de colocar imagenes que no esten rotas

--- DC1/src/components/FuncionalComponents/NavBarComponent/CartWidgetComponent/CartWidget.jsx
+++ DC1/src/components/FuncionalComponents/NavBarComponent/CartWidgetComponent/CartWidget.jsx
@@ -4,17 +4,19 @@ import { CartContext } from "../../../../context/CartContex";
 import { Link } from "react-router-dom";
 
 export default function CartWidget() {
-    const { cart } = useContext(CartContext);
-    return (
-        <Link to={'/cart'}>
-            <div>
-                <img
-                    className="LogoCarrito"
-                    src="https://cdn-icons-png.flaticon.com/512/107/107831.png"
-                    alt=""
-                />
-                <p className="NumeroCarrito">${cart.reduce((acc,curr)=>acc + curr.price*curr.quantify,0)} </p>
-            </div>
-        </Link>
-    );
+  const { cart } = useContext(CartContext);
+  return (
+    <Link to={"/cart"}>
+      <div>
+        <img
+          className="LogoCarrito"
+          src="https://cdn-icons-png.flaticon.com/512/107/107831.png"
+          alt=""
+        />
+        <p className="NumeroCarrito">
+          ${cart.reduce((acc, curr) => acc + curr.price * curr.quantify, 0)}{" "}
+        </p>
+      </div>
+    </Link>
+  );
 }
